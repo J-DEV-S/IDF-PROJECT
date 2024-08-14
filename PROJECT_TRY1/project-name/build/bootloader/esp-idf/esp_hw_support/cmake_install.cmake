4bd20d2e875d6741d16c1e93a4247a453c679647
J-DEV-S <142815859+J-DEV-S@users.noreply.github.com>
2024-08-14 19:49:37 +0530

    http code added
    tried with postman and worked!!!

--- PROJECT_TRY1/project-name/build/bootloader/esp-idf/esp_hw_support/cmake_install.cmake
+++ PROJECT_TRY1/project-name/build/bootloader/esp-idf/esp_hw_support/cmake_install.cmake
@@ -34,11 +34,11 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Espressif/tools/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin/xtensa-esp32-elf-objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Espressif/tools/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin/xtensa-esp32s2-elf-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/SANTHARAJ/IDF-Files/IDF-PROJECT/PROJECT_TRY1/project-name/build/bootloader/esp-idf/esp_hw_support/port/esp32/cmake_install.cmake")
+  include("C:/Users/SANTHARAJ/IDF-Files/IDF-PROJECT/PROJECT_TRY1/project-name/build/bootloader/esp-idf/esp_hw_support/port/esp32s2/cmake_install.cmake")
 endif()
 
